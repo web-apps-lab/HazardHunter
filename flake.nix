42b80b6c95b9ba9843b7045ed594c1f5dd5e8eb9
Fabien Boucher <web.apps.top@gmail.com>
2023-03-05 14:48:42 +0000

    Bump butler

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       "github:podenv/hspkgs/ba5d181089900f376f765e4a6889bd30c4f96993";
     flake-utils.url = "github:numtide/flake-utils";
     butler.url =
-      "github:TristanCacqueray/haskell-butler/acb47a5b2b3d913ec24439514f43baa0fbfde130";
+      "github:TristanCacqueray/haskell-butler/68cde9a74c530c7652c1dadf9a487b35ec0d0ab4";
   };
 
   outputs = { self, hspkgs, flake-utils, butler }:
